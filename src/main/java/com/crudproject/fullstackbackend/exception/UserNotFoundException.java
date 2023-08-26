2e77be9b1dce3e919d4a4c522579e77bc39df004
marlonhub <marlon.cuna123@gmail.com>
2023-08-25 22:12:01 -0400

    Updated backend

---
+++ src/main/java/com/crudproject/fullstackbackend/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.crudproject.fullstackbackend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+
+    public UserNotFoundException(Long id) {
+        super("Could not find the user with id " + (id));
+    }
+}
